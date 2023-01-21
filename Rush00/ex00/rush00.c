380500c7f659575110a55321801350f6ab7346d0
Quang Thang Pham <qupham@c3r7s3.42prague.com>
2023-01-21 19:58:37 +0100

    init

---
+++ Rush00/ex00/rush00.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fal-raye <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/15 11:48:23 by fal-raye          #+#    #+#             */
+/*   Updated: 2022/10/15 13:26:41 by fal-raye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_print_row(int length, char start, char mid, char end)
+{
+	int	row;
+
+	row = 1;
+	while (row <= length)
+	{
+		if (row == 1)
+		{
+			ft_putchar(start);
+		}
+		else if (row == length)
+		{
+			ft_putchar(end);
+		}
+		else
+		{
+			ft_putchar(mid);
+		}
+		row++;
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	parser;
+
+	parser = 1;
+	if (x >= 1 && y >= 1)
+	{
+		while (parser <= y)
+		{
+			if (parser == 1)
+			{
+				ft_print_row(x, 'o', '-', 'o');
+			}
+			else if (parser == y)
+			{
+				ft_print_row(x, 'o', '-', 'o');
+			}
+			else
+			{
+				ft_print_row(x, '|', ' ', '|');
+			}
+			parser++;
+		}
+	}
+}
